6a8c9dff00374cab90f01a5eb7027475a9f82ebe
V-jeetkhadse <jeet.khadse007@gmail.com>
2023-09-12 22:49:30 +0530

    Project Created

---
+++ Git_Task_2/src/main/java/com/Bharat/GitTask2Application.java
@@ -0,0 +1,13 @@
+package com.Bharat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTask2Application.class, args);
+	}
+
+}
